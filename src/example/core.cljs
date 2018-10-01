8c393c5110bac1f75b3dd0e905af14ae13620fce
Teodor Lunaas Heggelund <th@purelogic.no>
2018-10-01 16:43:12 +0200

    Remove alert and add "target" to :paths

--- src/example/core.cljs
+++ src/example/core.cljs
@@ -1,5 +1,3 @@
 (ns example.core)
 
-(let [msg "Change msg in src/example/core.cljs and see what happens!"]
-
-  (js/alert msg))
+(js/console.log "Change msg in src/example/core.cljs and see what happens!")
